fb293a847fe577b4b76e98391af6ad093f3cf401
Heiner Sameisky <hei.sam@gmail.com>
2013-09-03 20:39:38 +0200

    wip

--- lib/rails_encrypted_store.rb
+++ lib/rails_encrypted_store.rb
@@ -1,5 +1,5 @@
 require 'gibberish'
-
+require 'active_support/core_ext/hash/indifferent_access'
 require "rails_encrypted_store/version"
 require "rails_encrypted_store/railtie" if defined?(Rails::Railtie)
 
@@ -7,54 +7,118 @@ module RailsEncryptedStore
   extend ActiveSupport::Concern
 
   included do
-    before_save :encrypt_store
+    class_attribute :encrypted_attributes, instance_accessor: false
+    self.encrypted_attributes = {}
   end
 
   module ClassMethods
 
-    def encrypted_store(options={})
-      attr_accessor :decrypted_data
-      serialize :encrypted_data, Hash
-      decrypted_data_accessor(options[:accessors]) if options.has_key? :accessors
+    def encrypted_store(store_attribute, options = {})
+      attr_accessor "plain_#{store_attribute}"
+      serialize store_attribute, IndifferentCoder.new(options[:coder])
+      store_accessor(store_attribute, options[:accessors]) if options.has_key? :accessors
     end
 
-    def decrypted_data_accessor(*keys)
-      Array(keys).flatten.each do |key|
-        define_method("#{key}=") do |value|
-          self.decrypted_data = {} unless decrypted_data.is_a?(Hash)
-          decrypted_data[key] = value
-          self.encrypted_data_will_change!
-        end
+    def store_accessor(store_attribute, *keys)
+      keys = keys.flatten
 
-        define_method(key) do
-          self.decrypted_data = {} unless decrypted_data.is_a?(Hash)
-          decrypted_data[key]
+      _store_accessors_module.module_eval do
+        keys.each do |key|
+          define_method("#{key}=") do |value|
+            write_store_attribute(store_attribute, key, value)
+          end
+
+          define_method(key) do
+            read_store_attribute(store_attribute, key)
+          end
         end
       end
+
+      self.encrypted_attributes[store_attribute] ||= []
+      self.encrypted_attributes[store_attribute] |= keys
     end
+
+    def _store_accessors_module
+      @_store_accessors_module ||= begin
+        mod = Module.new
+        include mod
+        mod
+      end
+    end
+
   end
 
-  def encrypt_store
-    return unless decrypted_data
-    self.encrypted_data = {} unless encrypted_data.is_a?(Hash)
+  def encrypt_attribute(store_attribute, key, value)
     cipher = ::Gibberish::AES.new(self.aes_key)
-    decrypted_data.try(:each) do |key, value|
-      if value
-        encrypted_data[key] = cipher.enc(value)
-        send("encrypted_data_will_change!")
-      else
-        encrypted_data[key].delete
-      end
-    end
+    self.send(store_attribute)[key] = cipher.enc(value)
   end
 
-  def decrypt_store(aes_key=nil)
+  def decrypt_store(store_attribute, aes_key=nil)
     self.aes_key ||= aes_key
-    self.decrypted_data = {} unless decrypted_data.is_a?(Hash)
+    attribute = initialize_store_attribute("plain_#{store_attribute}")
     cipher = ::Gibberish::AES.new(self.aes_key)
-    encrypted_data.each do |key, value|
-      decrypted_data[key] = cipher.dec(encrypted_data[key]).force_encoding('UTF-8')
+    send(store_attribute).each do |key, value|
+      attribute[key] = cipher.dec(encrypted_data[key]).force_encoding('UTF-8')
     end
   end
+
+  private
+
+    def read_store_attribute(store_attribute, key)
+      attribute = initialize_store_attribute("plain_#{store_attribute}")
+      attribute[key]
+    end
+
+    def write_store_attribute(store_attribute, key, value)
+      attribute = initialize_store_attribute("plain_#{store_attribute}")
+      if !value
+        attribute[key].delete
+        self.send(store_attribute)[key].delete
+      end
+      if value != attribute[key]
+        send :"#{store_attribute}_will_change!"
+        attribute[key] = value
+        encrypt_attribute(store_attribute, key, value)
+      end
+    end
+
+    def initialize_store_attribute(store_attribute)
+      attribute = send(store_attribute)
+      unless attribute.is_a?(ActiveSupport::HashWithIndifferentAccess)
+        attribute = IndifferentCoder.as_indifferent_hash(attribute)
+        send :"#{store_attribute}=", attribute
+      end
+      attribute
+    end
+
+    class IndifferentCoder # :nodoc:
+      def initialize(coder_or_class_name)
+        @coder =
+          if coder_or_class_name.respond_to?(:load) && coder_or_class_name.respond_to?(:dump)
+            coder_or_class_name
+          else
+            ActiveRecord::Coders::YAMLColumn.new(coder_or_class_name || Object)
+          end
+      end
+
+      def dump(obj)
+        @coder.dump self.class.as_indifferent_hash(obj)
+      end
+
+      def load(yaml)
+        self.class.as_indifferent_hash @coder.load(yaml)
+      end
+
+      def self.as_indifferent_hash(obj)
+        case obj
+        when ActiveSupport::HashWithIndifferentAccess
+          obj
+        when Hash
+          obj.with_indifferent_access
+        else
+          ActiveSupport::HashWithIndifferentAccess.new
+        end
+      end
+    end
 
 end
--- test/rails_encrypted_store_test.rb
+++ test/rails_encrypted_store_test.rb
@@ -22,7 +22,7 @@ class Secret < ActiveRecord::Base
 
   include RailsEncryptedStore
 
-  encrypted_store accessors: [:author, :message]
+  encrypted_store :encrypted_data, accessors: [:author, :message]
 
   def aes_key
     self.encrypted_data_will_change!
@@ -37,18 +37,27 @@ class ActiveRecordTest < Test::Unit::TestCase
   def setup
     ActiveRecord::Base.connection.tables.each { |table| ActiveRecord::Base.connection.drop_table(table) }
     create_tables
+    @secret = Secret.create author: 'Heiner Sameisky', message: 'Lorem ipsum dolor sit amet'
   end
 
-  def test_should_encrypt_secret
-    @secret = Secret.create author: 'Heiner Sameisky', message: 'Lorem ipsum dolor sit amet'
-    @secret = Secret.find @secret.id
+  # def test_should_create_secret
+  #   @secret = Secret.create author: 'Heiner Sameisky', message: 'Lorem ipsum dolor sit amet'
+  #   #@secret = Secret.find @secret.id
+  # end
 
-    assert_nil @secret.author
+  def test_secret_is_encrypted
+    @secret = Secret.first
     assert_not_nil @secret.encrypted_data
+    assert_nil @secret.author
     assert_not_equal 'Heiner Sameisky', @secret.encrypted_data[:author]
-    
-    @secret.decrypt_store
+  end
+
+  def test_secret_can_be_decrypted
+    @secret = Secret.first
+    @secret.decrypt_store(:encrypted_data)
     assert_not_nil @secret.author
     assert_equal 'Heiner Sameisky', @secret.author
   end
+
+
 end
